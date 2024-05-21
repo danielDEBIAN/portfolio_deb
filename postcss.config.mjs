a3bb2545405dd7f23dbdeffed1200b7bb69dd3e0
Daniel Espinoza <espinoza.bautista.daniel246@gmail.com>
2024-05-21 00:56:10 -0600

    Solving warnings

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,5 @@
 /** @type {import('postcss-load-config').Config} */
 
-module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ]
-}
-
 const config = {
   plugins: {
     tailwindcss: {},
